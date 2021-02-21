a167e2d46a4e0b7d07c69485e014b521404c9e84
rsowald <72467920+rsowald@users.noreply.github.com>
2021-02-21 00:17:03 -0800

    added required packages and prompt questions

--- index.js
+++ index.js
@@ -1,13 +1,75 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const util = require('util');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [(async () => {
+    const data = await prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your GitHub username?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is your email address?'
+        },
+        {
+            type: 'input',
+            name: 'title',
+            message: "What is your project's name?"
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please write a short description of your project.'
+        },
+        {
+            type: 'input',
+            name: 'url',
+            message: 'If your project is deployed, what is the URL of the deployed application?'
+        },
+        {
+            type: 'list',
+            name: 'license',
+            message: 'What kind of license should your project have?',
+            choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+            default: 'none'
+        },
+        {
+            type: 'input',
+            name: 'install',
+            message: 'What command should be run to install dependencies?',
+            default: 'npm i'
+        },
+        {
+            type: 'input',
+            name: 'tests',
+            message: 'What command should be entered to run tests?',
+            default: 'npm test'
+        },
+        {
+            type: 'input',
+            name: 'using',
+            message: 'What steps should the user take to use the repo?'
+        },
+        {
+            type: 'input',
+            name: 'contributing',
+            message: 'How should the user contribute to the repo?'
+        }
+    ])
+}
+
+)];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
