5d84163178f1323483caea2bf448c83a1b16c98e
rsowald <72467920+rsowald@users.noreply.github.com>
2021-02-22 10:27:49 -0800

    added template literal for the generateMarkdown

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,56 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  
+  ## Description
+          
+  ${data.description}
+  
+  ## Table of Contents
+  
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Tests](#tests)
+  * [Contributing](#contributing)
+  * [License](#license)
+  * [Questions](#questions)
+    
+  ## Installation
+  ${data.install}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## License
+  ![License](https://img.shields.io/badge/License-${data.license}-green.svg "License Badge")
+  
+  ## Contributing
+  ${data.contributing}
 
-`;
+  Thank you to the following who have already contributed: ${data.contributors}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## Questions
+  
+  For more information:
+  Visit my GitHub: [https://${data.name}.github.io](https://${data.name}.github.io)
+
+  or Email: ${data.email}`;
 }
 
 module.exports = generateMarkdown;
